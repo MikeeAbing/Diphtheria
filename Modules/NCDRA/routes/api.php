fa946545c9d7652a44a14a152175598939ec7c25
Seigi Takada <choji.takada@gmail.com>
2025-04-10 18:22:40 +0800

    Remove NCDRA module

--- Modules/NCDRA/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\NCDRA\Http\Controllers\NCDRAController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('ncdra', NCDRAController::class)->names('ncdra');
-});
--- Modules/NCDRA/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\NCDRA\Http\Controllers\NCDRAController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('ncdra', NCDRAController::class)->names('ncdra');
-});
